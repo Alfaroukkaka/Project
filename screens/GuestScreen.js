1301d3931c40283bfcefb277ce944f750c50668f
MahmoudHusamAbdeen <mahmoodhusamabdeen2004@gmail.com>
2025-08-02 15:09:53 +0400

    Update GuestScreen.js

--- screens/GuestScreen.js
+++ screens/GuestScreen.js
@@ -1,9 +1,7 @@
-// GuestScreen.js
 import React, { useEffect } from 'react';
 import { View, Text, StyleSheet, ImageBackground } from 'react-native';
 
-const backgroundUri =
-  'https://sdmntprpolandcentral.oaiusercontent.com/files/00000000-921c-620a-af98-8aad4bc18e75/raw?se=2025-07-28T21%3A20%3A26Z&sp=r&sv=2024-08-04&sr=b&scid=dfc6604b-bafa-5d4b-9345-ab7ec4f8e607&skoid=a3412ad4-1a13-47ce-91a5-c07730964f35&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2025-07-28T18%3A06%3A40Z&ske=2025-07-29T18%3A06%3A40Z&sks=b&skv=2024-08-04&sig=mmdQBfXRs7Lj0oawM9bB0iG/Apj/eLBFsmCKhmAq7nw%3D';
+const backgroundUri = 'https://sdmntprpolandcentral.oaiusercontent.com/files/00000000-921c-620a-af98-8aad4bc18e75/raw?se=2025-07-28T22%3A31%3A39Z&sp=r&sv=2024-08-04&sr=b&scid=6d9d1348-659c-543f-b3c9-9a056b4dadb6&skoid=1e6af1bf-6b08-4a04-8919-15773e7e7024&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2025-07-28T19%3A29%3A10Z&ske=2025-07-29T19%3A29%3A10Z&sks=b&skv=2024-08-04&sig=XzlWCkpkIXxTAW/KdVDRLSff3wLkc8QMpA3siJNiCHU%3D';
 
 export default function GuestScreen({ navigation }) {
   useEffect(() => {
@@ -11,13 +9,12 @@ export default function GuestScreen({ navigation }) {
       name: 'Guest',
       type: 'Guest',
     };
-
     navigation.replace('FoodInteraction', {
       userData: guestUserData,
       isGuest: true,
     });
   }, [navigation]); // ✅ Fix: include navigation in dependency array
-
+  
   return (
     <ImageBackground source={{ uri: backgroundUri }} style={styles.background}>
       <View style={styles.container}>
@@ -30,6 +27,7 @@ export default function GuestScreen({ navigation }) {
 const styles = StyleSheet.create({
   background: {
     flex: 1,
+    resizeMode: 'cover',
   },
   container: {
     flex: 1,
